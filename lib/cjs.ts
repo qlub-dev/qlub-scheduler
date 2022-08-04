fbcd05cdf80907245da6d3d7bd7efe379b912f8b
halil <halillbaydar@gmail.com>
2022-08-04 20:54:08 +0300

    agenda module exported

--- lib/cjs.ts
+++ lib/cjs.ts
@@ -2,6 +2,8 @@
 
 import { Agenda } from "./agenda";
 import { SchedulerService } from "./agenda/scheduler.service";
+import { AgendaModule } from "./module";
 module.exports = Agenda;
 module.exports.Agenda = Agenda;
 module.exports.SchedulerService = SchedulerService;
+module.exports.SchedulerModule = AgendaModule;
