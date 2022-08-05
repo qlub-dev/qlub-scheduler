d010888c7350d832b9de1d1dff2f96675f61dd6d
halil <halillbaydar@gmail.com>
2022-08-05 13:34:34 +0300

    declare removed from scheduler expression

--- lib/cjs.ts
+++ lib/cjs.ts
@@ -6,4 +6,5 @@ export {
   QSchedulerExpression,
   EVERY_FIRST_DAY_OF_MOUNTH_AT,
   EVERY_DAY_AT,
+  Time,
 } from "../lib/utils/QSchedulerExpression";
--- lib/index.ts
+++ lib/index.ts
@@ -12,4 +12,5 @@ export {
   QSchedulerExpression,
   EVERY_FIRST_DAY_OF_MOUNTH_AT,
   EVERY_DAY_AT,
+  Time,
 } from "../lib/utils/QSchedulerExpression";
