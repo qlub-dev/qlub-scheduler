a84352355ab23ba7b480fe4b54a5991fa76f562f
= <=>
2022-11-21 15:57:00 +0300

    joblog exported

--- lib/cron-log/index.ts
+++ lib/cron-log/index.ts
@@ -1,12 +1,3 @@
 import { JobLog } from "./job.log.entity";
-import { JobLogServiceImpl } from "./job.log.service.impl";
 
-class QJobLog {
-  JobLog!: typeof JobLog;
-  JobLogService!: typeof JobLogServiceImpl;
-}
-
-QJobLog.prototype.JobLog = JobLog;
-QJobLog.prototype.JobLogService = JobLogServiceImpl;
-
-export { QJobLog };
+export { JobLog };
